3e20f200d47f0e5034888d8553438068b5ffcd78
Hanna Koh <hjk5643@g.rit.edu>
2025-02-22 14:43:39 -0500

    fridge door

--- TigerBites/src/App.tsx
+++ TigerBites/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from './components/ui/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className='base'>
