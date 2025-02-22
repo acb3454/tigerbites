7ab5863f8bd8b80c8adbb24472a3ef0eca15de0d
Peyton Wagner <paw7341@g.rit.edu>
2025-02-22 18:16:06 -0500

    services

---
+++ TigerBites/src/services/foodServices.tsx
@@ -0,0 +1,26 @@
+import { firestore } from "../config/firebase.ts";
+import { collection, getDocs, DocumentData, Timestamp } from "firebase/firestore";
+import { Food } from "../types/firebaseTypes.tsx";
+
+export async function getFoodItems(): Promise<Food[]> {
+    const foodCollection = collection(firestore, "food");
+    const snapshot = await getDocs(foodCollection);
+
+    return snapshot.docs.map((doc) => {
+        const data = doc.data() as DocumentData;
+
+        return {
+            id: doc.id,
+            contact: data.contact,
+            name: data.name,
+            description: data.description,
+            mealsAvailable: data['meals available'],
+            startPickup: data['start pickup'] instanceof Timestamp 
+                ? data['start pickup'].toDate() 
+                : data['start pickup'] || null,
+            endPickup: data['end pickup'] instanceof Timestamp 
+                ? data['end pickup'].toDate() 
+                : data['end pickup'] || null,
+        } as Food;
+    });
+}
