bdd1ae8e9c76b9661eca1913ac661bdeefa46945
Aemilia <acb3454@rit.edu>
2025-02-22 15:58:51 -0500

    firebase setup complete

--- TigerBites/src/App.tsx
+++ TigerBites/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/ui/navbar'
+import Home from './components/ui/home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <div className='base'>
         <NavBar></NavBar>
+        <Home></Home>
       </div>
     </>
   )
---
+++ TigerBites/src/components/ui/home.tsx
@@ -0,0 +1,34 @@
+import { firestore } from "../../config/firebase";
+import { addDoc, collection } from "@firebase/firestore";
+import React, { useRef } from "react";
+
+export default function home() {
+
+    const messageRef = useRef(null);
+    const ref = collection(firestore, "messages");
+
+
+    const handleSave = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        
+        let data = {
+            
+            message: "hello"
+        }
+        try {
+            addDoc(ref, data);
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSave}>
+                <label> Enter Message: </label>
+                <input type="text" ref={messageRef}/>
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    )
+}
