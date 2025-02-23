636defcb529421dbd7b5a4d74a15efba8a1c754c
Hanna Koh <hjk5643@g.rit.edu>
2025-02-23 00:39:59 -0500

    add take meal button

--- TigerBites/src/components/meal-details.tsx
+++ TigerBites/src/components/meal-details.tsx
@@ -3,7 +3,11 @@ import { useState, useEffect } from 'react';
 import { getMealById } from "../services/foodServices.tsx";
 import { Food } from "../types/firebaseTypes.tsx";
 import InsideFridge from '@/components/inside-fridge';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, redirect } from 'react-router-dom';
+import { useAuth } from '@/hooks/useAuth.ts';
+import { Button } from './ui/button.tsx';
+import { doc, setDoc } from 'firebase/firestore';
+import { firestore } from '@/config/firebase.ts';
 
 // Convert timestamps
 const formatTimestamp = (timestamp: any) => {
@@ -16,6 +20,7 @@ export default function MealDetails() {
     const [loading, setLoading] = useState(true);
     const { mealId } = useParams(); // Get mealId from URL
     const navigate = useNavigate();
+    const {userData} = useAuth();
 
     useEffect(() => {
         if (!mealId) {
@@ -27,12 +32,12 @@ export default function MealDetails() {
             try {
                 const data = await getMealById(mealId);
                 const mealsAvailable = data?.mealsAvailable;
-                if (mealsAvailable){
-                    if (mealsAvailable>0){
+                if (mealsAvailable) {
+                    if (mealsAvailable > 0) {
                         setMeal(data);
                     }
                 }
-                
+
             } catch (error) {
                 console.error("Error fetching meal details:", error);
             } finally {
@@ -51,11 +56,47 @@ export default function MealDetails() {
         </div>
     );
 
+    const updateUser = async () => {
+        if (!userData) {
+            redirect('/login');
+        }
+        const userRef = doc(firestore, 'users', userData!.id);;
+        const mealNum = userData!.meals_taken + 1;
+        try {
+            await setDoc(userRef, { meals_taken: mealNum }, { merge: true });
+        } catch (error) {
+            console.error('Error updating user data:', error);
+        }
+    }
+
+    const takeMeal = async () => {
+        if (!meal) {
+            throw new Error('Meal data is not available.');
+        }
+        const mealRef = doc(firestore, 'food', mealId!);
+        const mealCount = meal.mealsAvailable - 1;
+        try {
+            await setDoc(mealRef, { mealsAvailable: mealCount }, { merge: true });
+            await updateUser();
+            const data = await getMealById(mealId!);
+            const mealsAvailable = data?.mealsAvailable;
+            if (mealsAvailable) {
+                if (mealsAvailable > 0) {
+                    setMeal(data);
+                }
+            }
+        } catch (error) {
+            console.error('Error updating meal data:', error);
+        }
+        console.log("Meal taken");
+    };
+
     return (
         <InsideFridge>
             <div className='detail-page'>
-                <button onClick={() => navigate(-1)}>Back</button>
+                <Button onClick={() => navigate(-1)} className='button1'>Back</Button>
                 <h2><strong>{meal.name}</strong></h2>
+                <p>{meal.location}</p>
                 <div className='meal-info'>
                     <p>{meal.description}</p>
                     <p>Meals Available: <strong>{meal.mealsAvailable}</strong></p>
@@ -64,6 +105,7 @@ export default function MealDetails() {
                     Pickup: {formatTimestamp(meal.startPickup)} - {formatTimestamp(meal.endPickup)}
                 </p>
                 {meal.imageUrl && <img src={meal.imageUrl} alt={meal.name} />}
+                <Button className='button2' onClick={takeMeal}>Take Meal</Button>
             </div>
         </InsideFridge>
     );
