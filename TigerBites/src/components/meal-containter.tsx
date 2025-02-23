deefe55735028ea5651960cbd7590a3d235d0d55
Hanna Koh <hjk5643@g.rit.edu>
2025-02-23 00:46:07 -0500

    fix vercel error for real i hope

--- TigerBites/src/components/meal-containter.tsx
+++ TigerBites/src/components/meal-containter.tsx
@@ -4,8 +4,11 @@ import { getFoodItems } from "../services/foodServices.tsx";
 import { Food } from "../types/firebaseTypes.tsx";
 import { useNavigate } from 'react-router-dom';
 
+interface MealContainerProps {
+    onMealClick: (id: string) => void;
+}
 
-export default function MealContainer() {
+export default function MealContainer({}: MealContainerProps) {
     const [foodItems, setFoodItems] = useState<Food[]>([]);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
